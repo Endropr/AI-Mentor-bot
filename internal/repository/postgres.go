634fa26c2cb301d5f74b2f6b2f38b7e37406f949
KASTA <76002509+Endropr@users.noreply.github.com>
2026-02-14 19:49:47 +0500

    Refactor PostgresRepo

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -6,7 +6,6 @@ import (
 	"github.com/Endropr/ai-programming-mentor/internal/domain"
 )
 
-// PostgresRepo будет отвечать за связь Go с твоей базой ai_mentor_db
 type PostgresRepo struct {
 	conn *pgx.Conn
 }
@@ -15,11 +14,9 @@ func NewPostgresRepo(conn *pgx.Conn) *PostgresRepo {
 	return &PostgresRepo{conn: conn}
 }
 
-// SaveMessage автоматически выполнит INSERT, который ты до этого делал вручную
 func (r *PostgresRepo) SaveMessage(ctx context.Context, msg domain.Message) error {
 	query := `INSERT INTO messages (user_id, role, content, selected_language) VALUES ($1, $2, $3, $4)`
 	
-	// Передаем значение из структуры domain.Message в запрос
 	_, err := r.conn.Exec(ctx, query, msg.UserID, msg.Role, msg.Content, msg.SelectedLanguage)
 	return err
 }
